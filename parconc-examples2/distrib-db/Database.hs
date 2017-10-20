f1851704f9ab7c49b5d31e9e98df62484fb77bac
Peter Becich <peterbecich@gmail.com>
2017-10-19 19:06:14 -0700

    wip

--- parconc-examples2/distrib-db/Database.hs
+++ parconc-examples2/distrib-db/Database.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE TemplateHaskell, DeriveDataTypeable, DeriveGeneric #-}
+{-# LANGUAGE RecordWildCards #-}
+
 module Database (
        Database,
        Key, Value,
@@ -6,24 +9,72 @@ module Database (
        rcdata,
   ) where
 
-import Control.Distributed.Process
+
+import GHC.Generics (Generic)
+import Data.Typeable
+import Data.Binary hiding (get)
+
+import Control.Distributed.Process hiding (Message)
 
 import qualified Data.Map as Map
-import Data.Map (Map)
+import Data.Map (Map, insert, empty)
+
+import Control.Monad
 
 type Key   = String
 type Value = String
 
-type Database = ProcessId
+-- type Database = ProcessId
+data Database = Database
+  { storage :: Map Key Value
+  , spid :: ProcessId
+  }
+
+-- replace with Lens
+insertkv :: Database -> Key -> Value -> Database
+insertkv db k v = let
+  sp = spid db
+  st = storage db
+  st' = insert k v st
+  in Database st' sp
+
+data Request = Set Key Value
+             | Get Key
+  deriving (Typeable, Generic)
+
+type Response = Maybe Value
+
+instance Binary Request
+-- instance Binary Response
+
+-- needless IO?
+-- handleRequest :: Database -> Request -> IO (Database, Response)
+-- handleRequest db (Set k v) = do
+--    db' <- pure $ insertkv db k v
+--    let response = Just v
+--    pure (db', response)
+
+handleRequest :: Database -> Request -> (Database, Response)
+handleRequest db (Set k v) = let
+  db' = insertkv db k v
+  response = Just v
+  in (db', response)
+handleRequest db (Get k) = (db, Map.lookup k (storage db))
 
 createDB :: [NodeId] -> Process Database
-createDB nodes = error "not implemented!" -- exercise
-
+createDB nodes = do
+  pid <- getSelfPid
+  -- put storage here?  KV Map?
+  pure $ Database empty pid
+  
 set :: Database -> Key -> Value -> Process ()
-set db k v = error "not implemented!" -- exercise
+set db k v = do
+  send (spid db) $ Set k v
 
-get :: Database -> Key -> Process (Maybe Value)
-get db k = error "not implemented!" -- exercise
+get :: Database -> Key -> Process Response
+get db k = do
+  send (spid db) $ Get k
+  expect
 
 rcdata :: RemoteTable -> RemoteTable
 rcdata = id
